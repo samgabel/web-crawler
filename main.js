57bbdb990c2c5fafc66d0a3e442e1e466476f334
Sam Gabel <sam@gabelcloud.com>
2024-05-30 21:11:36 -0700

    feat: process cli arguments in main
    Signed-off-by: Sam Gabel <sam@gabelcloud.com>

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log('Hello world!')
+// https://nodejs.org/docs/latest/api/process.html#processargv
+import { argv } from 'node:process'
+
+
+
+function main() {
+  const args = argv.slice(2)
+  if (args.length < 1) {
+    console.log('No arguments supplied')
+  } else if (args.length > 1) {
+    console.log('Too many arguments supplied')
+  } else {
+    console.log(`The Web Crawler is starting on ${args[0]}`)
+  }
+}
+
+main()
